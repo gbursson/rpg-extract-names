1a28c08332a5d9bf4078f9e2276c46b80dfb24c4
Gbursson <gbursson@gmail.com>
2018-02-21 19:49:27 +0000

    added a filter for non-alphanumeric characters

--- extract_names.py
+++ extract_names.py
@@ -4,22 +4,25 @@ import operator
 
 file = open("files/a.html", "r", encoding="utf-8")
 
-remove_text = "(),\„"
-remove_list = list(remove_text)
-
 text = BeautifulSoup(file, "html.parser").get_text()
-
 text = text.split(" ")
-text = sorted(list(text))
+
+
+new_list = []
+for word in text:
+    word = re.sub('[!,()„*.\d+?—]', '', word)
+    new_list.append(word)
+
+text = new_list
 
 text = list(filter(lambda x: x != '', text))
 text = list(filter(lambda x: x.istitle(), text))
 text = list(filter(lambda x: (len(x) != 1 and len(x) != 2), text))
 
+text = set(text)
+text = sorted(list(text))
 
 print(text)
-
-
 
 """
 for word in text:
