a90930dc75357c69de386279030b655f0a361f2b
Gbursson <gbursson@gmail.com>
2018-02-21 20:23:23 +0000

    added write to file.

--- extract_names.py
+++ extract_names.py
@@ -1,30 +1,32 @@
 from bs4 import BeautifulSoup
 import re
-import operator
 
-file = open("files/a.html", "r", encoding="utf-8")
+input_file = open("files/a.html", "r", encoding="utf-8")
 
-text = BeautifulSoup(file, "html.parser").get_text()
+text = BeautifulSoup(input_file, "html.parser").get_text()
+input_file.close()
+
 text = text.split(" ")
 
-
-new_list = []
-for word in text:
-    word = re.sub('[!,()„*.\d+?—]', '', word)
-    new_list.append(word)
-
-text = new_list
-
+# apply set of filters to the list (remove - empty, non-names and 1 or 2 characters)
 text = list(filter(lambda x: x != '', text))
 text = list(filter(lambda x: x.istitle(), text))
 text = list(filter(lambda x: (len(x) != 1 and len(x) != 2), text))
 
+# regexp non-letter characters
+
+new_list = []
+for word in text:
+    word = re.sub('[!,()„*.\d+?—“”:;]', '', word)
+    new_list.append(word)
+text = new_list
+
 text = set(text)
 text = sorted(list(text))
 
-print(text)
+output_file = open("namex.txt", "w+", encoding="utf-8")
 
-"""
 for word in text:
-    print(word)
-"""
+    output_file.write(word + "\n")
+
+output_file.close()
